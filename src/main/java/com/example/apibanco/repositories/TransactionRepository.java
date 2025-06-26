1648fe8adbe85ddcb9bf25d07bcfeada5df3fc36
Pedro Moreira <peedroroi@gmail.com>
2025-06-26 16:57:15 -0500

    update: criação de método para filtragem de dados no banco

--- src/main/java/com/example/apibanco/repositories/TransactionRepository.java
+++ src/main/java/com/example/apibanco/repositories/TransactionRepository.java
@@ -3,6 +3,9 @@ package com.example.apibanco.repositories;
 import com.example.apibanco.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+import java.time.OffsetDateTime;
+import java.util.List;
 
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    public List<Transaction> findByDataHoraAfter(OffsetDateTime dataHora);
 }
